a5a8d1a99e52bb9ccf6b896cfa4e60a8499ecb18
akhsanul044 <akhsanul044@gmail.com>
2026-06-09 17:17:14 +0700

    update

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const toggleButton = document.getElementById("theme-toggle");
 
 if(localStorage.getItem("theme") === "dark"){
     document.body.classList.add("dark-mode");
-    toggleButton.textContent = "L";
+    toggleButton.textContent = "D";
 }
 
 toggleButton.addEventListener("click", () => {
@@ -10,10 +10,10 @@ toggleButton.addEventListener("click", () => {
     document.body.classList.toggle("dark-mode");
 
     if(document.body.classList.contains("dark-mode")){
-        toggleButton.textContent = "L";
+        toggleButton.textContent = "D";
         localStorage.setItem("theme", "dark");
     }else{
-        toggleButton.textContent = "D";
+        toggleButton.textContent = "L";
         localStorage.setItem("theme", "light");
     }
 
